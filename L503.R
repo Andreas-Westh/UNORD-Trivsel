5f028b091c438ca254784a391f1982eeb0adeae6
60gCreatine <cphah643@gmail.com>
2025-06-04 13:43:08 +0200

    initial commit, working Feature Enginner for 90 days prior to start

---
+++ L503.R
@@ -0,0 +1,30 @@
+# Main goal of this script
+# Feature engineer the valid contract time period based on:
+# 1. Start date
+# 2. Contract signed date
+# 3. Todays date?
+
+
+
+# currently testing from local file, will later come from data lake 
+# and go into d ata warehouse
+
+library(readxl)
+
+
+L503 <- read_xlsx("data/Odin_L503_GF-elever_ med_aftale.xlsx")
+# Date format is Y-m-d
+
+# converts into unix timecode
+L503$Startdato_unix <- as.numeric(as.POSIXct(L503$Startdato, tz = "UTC"))
+L503$Underskriftsdato_unix<- as.numeric(as.POSIXct(L503$Underskriftsdato, tz = "UTC"))
+
+# calcualte 90 days into unix timecode
+unix_value_90_days <- 90*24*60*60
+
+# find earliest date for valid contract
+L503$Unix_90_days <- L503$Startdato_unix - unix_value_90_days
+# converts back to actual readable date format¬∆
+L503$Earliest_date <- format(as.POSIXct(L503$Unix_90_days, origin = "1970-01-01", tz = "UTC"), "%Y-%m-%d")
+
+
