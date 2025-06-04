10c64890d9aeb73e1508d197b36c1c36862d3269
60gCreatine <cphah643@gmail.com>
2025-06-04 14:42:25 +0200

    boolean funktion test for relation between 90 days before and date

--- L503.R
+++ L503.R
@@ -2,8 +2,7 @@
 # Feature engineer the valid contract time period based on:
 # 1. Start date
 # 2. Contract signed date
-# 3. Todays date?
-
+# 3. 
 
 
 # currently testing from local file, will later come from data lake 
@@ -24,7 +23,10 @@ unix_value_90_days <- 90*24*60*60
 
 # find earliest date for valid contract
 L503$Unix_90_days <- L503$Startdato_unix - unix_value_90_days
-# converts back to actual readable date format¬∆
+# converts back to actual readable date format
 L503$Earliest_date <- format(as.POSIXct(L503$Unix_90_days, origin = "1970-01-01", tz = "UTC"), "%Y-%m-%d")
 
+# get a boolean, to check, startdato can be changed for a different date
+L503$Kontrakt_rettidigt <- L503$Underskriftsdato_unix >= L503$Unix_90_days &
+  L503$Underskriftsdato_unix <= L503$Startdato_unix
 
